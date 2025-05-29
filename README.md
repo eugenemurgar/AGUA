657080ee6b5aa85c0420af9dcdf270f0738c264c
HelaGone <luah@gmail.com>
2025-05-28 20:20:46 -0600

    code implementation

---
+++ README.md
@@ -0,0 +1,14 @@
+# instucciones para subir cambio al repositorio
+git add .
+git commit -m 'mensaje'
+git push
+
+# Importante considerar lo siguiente:
+- para hacer push primero necesito tener todos los cambios del repositorio en mi local
+git pull
+
+# otros comandos útiles
+git status
+git stash
+git log --graph
+git checkout
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-document.addEventListener("DOMContentLoaded", function () {
-  console.log("DOM fully loaded and parsed");
+// document.addEventListener("DOMContentLoaded", function () {
+//   console.log("DOM fully loaded and parsed");
 
-  const icons = document.querySelectorAll(".icon");
-  const body = document.querySelector("body");
-  icons.forEach((icon) => {
-    icon.addEventListener("mouseenter", function () {
-      const clonedNode = this.cloneNode(true);
-      body.appendChild(clonedNode);
-    });
-  });
-});
+//   const icons = document.querySelectorAll(".icon");
+//   const body = document.querySelector("body");
+//   icons.forEach((icon) => {
+//     icon.addEventListener("mouseenter", function () {
+//       const clonedNode = this.cloneNode(true);
+//       body.appendChild(clonedNode);
+//     });
+//   });
+// });
